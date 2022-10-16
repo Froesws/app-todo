814e38982378829cf2d83618a21ee396934d68d6
William Froes <froes.ws@gmail.com>
2022-10-16 02:55:04 -0300

    Estilos cridos, botão deletar funcional

--- src/app/module/home/components/todo-list/todo-list.component.ts
+++ src/app/module/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList:Array<TaskList> = [
+    {task:"Minha Nova tast", checked:true},
+    {task:"Minha Nova tast 2", checked:false}];
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event,1);
+  }
 
+  public deleteAllTaskList(){
+    const confirm = window.confirm("Are you sure? All items will be removed.");
+    if(confirm){
+      this.taskList = [];
+    }
+  }
 }
+
